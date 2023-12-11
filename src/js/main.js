40badb8294be2736c9c1e2d6557bf0849c525783
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2023-12-11 21:06:33 +0100

    add animations stars and big dipper

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const stars = document.querySelectorAll(".star");
 
+	const resetAnimation = (star) => {
+		star.className = "";
+		setTimeout(function () {
+			star.className = "star";
+			// startShootingStarAnimation(star); // Dodaj to, aby natychmiast uruchomić nową animację
+		}, 0);
+	};
+
+	stars.forEach((star) => {
+		star.addEventListener("animationend", () => {
+			resetAnimation(star);
+		});
+	});
+});
