f890f850e3a3686adef14125be9e37b6202483ee
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2023-12-15 23:27:50 +0100

    menu burger done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
+const burger = document.querySelector(".nav__menuBurger");
+const menu = document.querySelector(".nav__list");
+// const nav = document.querySelector(".nav");
+// const links = document.querySelectorAll(".header__menu-link");
+
 document.addEventListener("DOMContentLoaded", function () {
 	const stars = document.querySelectorAll(".header__star");
 
@@ -15,3 +20,27 @@ document.addEventListener("DOMContentLoaded", function () {
 		});
 	});
 });
+
+const handleBurger = () => {
+	burger.classList.toggle("active");
+	menu.classList.toggle("showMenu");
+};
+
+// const changeBackgroundColor = () => {
+// 	if (window.scrollY >= 20) {
+// 		nav.style.backgroundColor = "#15191D";
+// 		nav.style.transition = "all .3s";
+// 	} else {
+// 		nav.style.backgroundColor = "transparent";
+// 	}
+// };
+
+burger.addEventListener("click", handleBurger);
+// window.addEventListener("scroll", changeBackgroundColor);
+
+// links.forEach((item) => {
+// 	item.addEventListener("click", () => {
+// 		burger.classList.remove("active");
+// 		menu.classList.remove("show-menu");
+// 	});
+// });
