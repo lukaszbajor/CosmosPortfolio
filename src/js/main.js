cf0497190e3b28f51b9e84396c1a439c79387e71
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2024-01-21 01:25:17 +0100

    smallDipper in nav for mobile and contact section

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 const burger = document.querySelector(".nav__menuBurger");
 const menu = document.querySelector(".nav__list");
 const nav = document.querySelector(".nav");
-// const links = document.querySelectorAll(".header__menu-link");
+const links = document.querySelectorAll(".nav__link");
 
 document.addEventListener("DOMContentLoaded", function () {
 	const stars = document.querySelectorAll(".header__star");
@@ -34,6 +34,13 @@ const changeBackgroundColor = () => {
 		nav.style.backgroundColor = "transparent";
 	}
 };
+
+links.forEach((link) => {
+	link.addEventListener("click", () => {
+		menu.classList.remove("showMenu");
+		burger.classList.remove("active");
+	});
+});
 
 burger.addEventListener("click", handleBurger);
 window.addEventListener("scroll", changeBackgroundColor);
